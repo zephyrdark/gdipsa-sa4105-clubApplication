c22930b029187a71d42b2cdd4dc39247ba380f99
Alvin Zhi Wei Lee <e1327927@u.nus.edu>
2024-05-30 11:54:54 +0800

    Completed Part1 and step6 of Part2

--- src/clubApplication/Club.java
+++ src/clubApplication/Club.java
@@ -1,56 +1,99 @@
 package clubApplication;
 
-public class Club {
-	// attributes
-	private Member[] members = new Member[0];
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+
+public class Club {	
+	// Attributes
+	
+	// --MEMBER RELATED--
+	private List<Member> members = new ArrayList<Member>();
 	private int currentNumber = 0;
 	
-	// methods
-	private Member[] extendMembersArray(Member[] arr) {
-		// increase members array length to accommodate new member
-		Member[] extendedMembersArray = new Member[arr.length + 1];
-		for (int i = 0; i < arr.length; i++) {
-			extendedMembersArray[i] = arr[i];
-		}
-		return extendedMembersArray;
-	}
+	// --FACILITY RELATED--
+	private Map<String, Facility> facilities = new HashMap<String, Facility>();
 	
+	
+	// Methods	
+	
+	// --MEMBER RELATED--
 	public Member addMember(Person person) {
-		// initialize new Member using Person, since Person object may already
+		// initialize new Member using Person, since Person object must already
 		// exist and we can get the attributes required for Member from Person.
 		Member newMember = new Member(currentNumber, person.getSurName(), 
 				person.getFirstName(), person.getSecondName());
-		// increase members array length to accommodate new member
-		members = extendMembersArray(members);
 		// add new member as last member;
-		members[currentNumber] = newMember;
-		// increment last member number for next new member;
-		currentNumber += 1;
+		members.add(newMember);
+		currentNumber++;
 		return newMember;
 	}
 	
+	// Removes a Member from this Club's instance, given a memberNumber integer.
 	public void removeMember(int memberNumber) {
-		Member[] resizedMembersArray = new Member[members.length-1];
-		for (int i = 0; i < members.length; i++) {
-			
-			// since member number is an integer, we can use comparison operators
-			// to check condition whether to add them to new array or not.
-			if (members[i].getMemberNumber() < memberNumber) {
-				resizedMembersArray[i] = members[i];
-			}
-			else if (members[i].getMemberNumber() > memberNumber) {
-				resizedMembersArray[i-1] = members[i];
+		for (Member member: members) {
+			if (member.getMemberNumber() == memberNumber) {
+				members.remove(member);
+				return;
 			}
 		}
-		members = resizedMembersArray;
 	}
 	
+	// Shows all members of this Club instance's members.
 	public void showMembers() {
-		// C# for each equivalent
+		// the below for loop is the equivalent of C#'s for-each
 		for (Member member: members) {
-			if (member != null) {
-				member.show();				
+			member.show();
+		}
+	}
+	
+	// Gets the list of members of this Club's instance.
+	public List<Member> getMembers() {
+		return members;
+	}
+	
+	// Finds a Member in this Club's instance, given a memberNumber integer.
+	public Member findMember(int memberNumber) {
+		for (Member member: members) {
+			if (member.getMemberNumber() == memberNumber) {
+				return member;
 			}
+		}
+		return null;
+	}
+	
+	// --FACILITY RELATED--
+	// Gets the Facility object given a facility name.
+	public Facility getFacility(String name) {
+		return facilities.get(name);
+	}
+	
+	// Gets all the facilities of this Club's instance.
+	public Map<String, Facility> getFacilities() {
+		return facilities;
+	}
+	
+	// Adds given facility object into this Club instance's facilities.
+	public Facility addFacility(Facility facility) {
+		facilities.put(facility.getName(), facility);
+		return facility;
+	}
+	
+	// Removes given facility name from this Club instance's facilities
+	public void removeFacility(String name) {
+		facilities.remove(name);
+	}
+
+	// Shows all facilities name and description from this Club instance's 
+	// facilities.
+	public void showFacilities() {
+		// Use Set and .keySet() to make the facilities K,V pairs readable.
+		Set<String> facilitiesKeys = facilities.keySet();
+		// Loop through each set and call its .show() method
+		for (String facility: facilitiesKeys) {
+			facilities.get(facility).show();
 		}
 	}
 }
--- src/clubApplication/ClubApplication.java
+++ src/clubApplication/ClubApplication.java
@@ -5,20 +5,30 @@ public class ClubApplication {
 	public static void main(String[] args) {
 		System.out.println("--ClubApplication main--");
 		
+		// Test Member and respective Club methods.
 		Club club1 = new Club();
 		club1.addMember(new Person("Lee", "Alvin"));
 		club1.addMember(new Person("Tan", "Ah", "Beng"));
 		club1.addMember(new Person("Stan", "Laurel"));
 		club1.showMembers();
 		
-		System.out.println("Remove Member 1");
+		System.out.println("\nRemove Member 1");
 		club1.removeMember(1);
-		club1.showMembers();		
-				
+		club1.showMembers();
+		
+		System.out.println("\nFind members 0 and 2");
+		System.out.println(club1.findMember(0));
+		System.out.println(club1.findMember(2));
+			
+		// Test Facility and respective Club methods.
 		Facility facility1 = new Facility("Gym");
 		Facility facility2 = new Facility("Spa","Treat yourself to a rejuvenating spa");
-		facility1.show();
-		facility2.show();
+				
+		club1.addFacility(facility1);
+		club1.addFacility(facility2);
+		
+		System.out.println("\nShow club Facilities:");
+		club1.showFacilities();
 	}
 
 }
